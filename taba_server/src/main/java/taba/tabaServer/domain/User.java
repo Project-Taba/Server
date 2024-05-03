00d2eb23cc3733aee0cc13f2791f6cc47aef9db0
changuk2 <changuk0308@naver.com>
2024-05-03 16:29:56 +0900

    Feat: 추가 도메인User

---
+++ taba_server/src/main/java/taba/tabaServer/domain/User.java
@@ -0,0 +1,73 @@
+package taba.tabaServer.domain;
+
+import taba.tabaServer.config.oauth.OAuthProvider;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name="user_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OAuthProvider oauthProvider;
+
+    @Column(name="user_name")
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    //Ouath2 기능을 위해 nullable=true로 설정
+    @Column(name = "password", nullable = true)
+    private String password;
+
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    @Column(name="birthyear", nullable = false)
+    private String birthyear;
+
+    @Column(name = "birthday", nullable = false)
+    private String birthday;
+
+    @Column(name = "mobile_number", nullable = false)
+    private String mobile;
+
+    //Ouath2 기능을 위해 nullable=true로 설정
+    @Column(name="created_at", nullable = true)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public User(
+            final String name,
+            final String email,
+            final String password,
+            final String gender,
+            final String birthday,
+            final String birthyear,
+            final String mobile,
+            final OAuthProvider oauthProvider
+    ) {
+        this.name=name;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.birthyear = birthyear;
+        this.createdAt = LocalDateTime.now();
+        this.mobile=mobile;
+        this.name=name;
+        this.oauthProvider=oauthProvider;
+    }
+}
