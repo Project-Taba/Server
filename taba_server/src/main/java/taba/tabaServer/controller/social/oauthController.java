ee6206d7f7ede1a5690ef6d23f7a763542dae5bd
changuk2 <changuk0308@naver.com>
2024-05-03 16:46:16 +0900

    Feat: 추가 소셜 로그인 관련 Controller

---
+++ taba_server/src/main/java/taba/tabaServer/controller/social/oauthController.java
@@ -0,0 +1,32 @@
+package taba.tabaServer.controller.social;
+
+import taba.tabaServer.config.AuthTokens;
+import taba.tabaServer.config.infra.kakao.KakaoLoginParams;
+import taba.tabaServer.config.infra.naver.NaverLoginParams;
+import taba.tabaServer.service.OAuthLoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/oauth")
+public class oauthController {
+    //oauth 로그인 서비스
+    private final OAuthLoginService oAuthLoginService;
+
+    //카카오 로그인
+    @PostMapping("/kakao")
+    public ResponseEntity<AuthTokens> loginKakao(@RequestBody KakaoLoginParams params) {
+        return ResponseEntity.ok(oAuthLoginService.login(params));
+    }
+    
+    //네이버 로그인
+    @PostMapping("/naver")
+    public ResponseEntity<AuthTokens> loginNaver(@RequestBody NaverLoginParams params) {
+        return ResponseEntity.ok(oAuthLoginService.login(params));
+    }
+}
